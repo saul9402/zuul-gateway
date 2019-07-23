6baa17042816e138d8f62851046f89f7bd3ef624
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 18:56:50 -0500

    se configura el gateway zuul para que funcione como puerta de enlace y asi tener un unico punto de entrada a los microservicios

--- src/main/java/com/formacionbdi/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.formacionbdi.springboot.app.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class SpringbootServicioZuulServerApplication {
 
 	public static void main(String[] args) {
